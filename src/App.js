b8cfd81ddf2088071320754552a14fde71afcbc6
ugsnikhilesh <gsnikhilesh.utukuri@gmail.com>
2023-06-19 12:41:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>20BCE1174
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
